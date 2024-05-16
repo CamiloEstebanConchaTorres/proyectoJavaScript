87d7b8ec83daf750c39492d49a2115ca339e62ce
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-05-16 12:10:56 -0500

    feat: :construction: Avance de estructuracion en el html y Css para dar mas contraste a el proyecto

--- Js/main.js
+++ Js/main.js
@@ -1,36 +1,34 @@
-const API_URL = 'https://spotify23.p.rapidapi.com';
-const API_KEY = '27affe6731msh64d1fa5c477d18dp1f41d5jsn3f8bf97a8340';
-const API_HOST = 'spotify23.p.rapidapi.com';
+class myframe extends HTMLElement{
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
 
-class myframe extends HTMLElement {
-  id;
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
+    connectedCallback() {
+        this.renderFrame();
+    }
 
-  async connectedCallback() {
-    const response = await fetch(`${API_URL}/tracks/?ids=${this.id}`, {
-      headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': API_HOST
-      }
-    });
-    const data = await response.json();
+    renderFrame() {
+        const uri = this.getAttribute('uri');
+        if (uri) {
+            const id = uri.split(':')[2];
+            const typeOf = uri.split(':')[1];
+            this.shadowRoot.innerHTML = `
+                <iframe class="spotify-iframe" width="100%" height="670" src="https://open.spotify.com/embed/${typeOf}/${id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            `;
+        } else {
+            this.shadowRoot.innerHTML = '';
+        }
+    }
 
-    this.shadowRoot.innerHTML = /*html*/`
-      <iframe class="spotify-iframe" width="454" height="690" src="${data.tracks[0].preview_url}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-    `;
-  }
+    static get observedAttributes() {
+        return ["uri"];
+    }
 
-  static get observedAttributes() {
-    return ["uri"];
-  }
-
-  attributeChangedCallback(name, old, now) {
-    let [nameUri, track, id] = now.split(":");
-    this.id = id;
-  }
+    attributeChangedCallback(name, oldVal, newVal) {
+        if (name === 'uri' && oldVal !== newVal) {
+            this.renderFrame();
+        }
+    }
 }
-
-customElements.define("my-frame", myframe);
+customElements.define("my-frame",myframe)
