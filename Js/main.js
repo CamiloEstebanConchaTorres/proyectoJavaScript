91eea80d77294164ec4ab111db49c2084e072c38
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-05-17 11:47:41 -0500

    feat: :construction: Implementacion de actualizacion del id del URI de la cancion para que se reproduzca en la mitad

--- Js/main.js
+++ Js/main.js
@@ -37,65 +37,74 @@ customElements.define("my-frame",myframe)
 //////////////////////// SEARCH SONGS RIGTH //////////////////////////////////////////////////
 
 document.addEventListener('DOMContentLoaded', function() {
-  let searchInput = document.getElementById('placeholdersearch'); // Ajustado para el nuevo ID del campo de entrada
+  let searchInput = document.getElementById('placeholdersearch'); 
   let songList = document.getElementById('Song-card');
+  let frame = document.querySelector('.Framecenter');
   let lastSearchTime = 0;
   const searchDelay = 100;
 
   async function searchSpotify(search) {
-      const currentTime = Date.now();
-      if (currentTime - lastSearchTime < searchDelay) {
-          console.log('Espera un momento antes de realizar otra búsqueda.');
-          return;
-      }
-
-      try {
-          let url = `https://spotify23.p.rapidapi.com/search/?q=${search}&type=tracks&offset=0&limit=8&numberOfTopResults=5`;
-          let res = await fetch(url, {
-              method: 'GET',
-              headers: {
-                  'X-RapidAPI-Key': '27affe6731msh64d1fa5c477d18dp1f41d5jsn3f8bf97a8340',
-                  'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-              },
-          });
-          let data = await res.json();
-          console.log(data);
-          if (data && data.tracks && data.tracks.items) {
-              songList.innerHTML = '';
-              data.tracks.items.forEach((song, index) => {
-                  console.log(`Processing song ${index}`, song);
-                  console.log('Detalles de la canción:', song);
-                  if (song && song.data && song.data.name && song.data.artists) {
-                      let imageUrl = song.data.albumOfTrack?.coverArt?.sources[0]?.url || '';
-                      let songItem = document.createElement('div');
-                      songItem.classList.add('song-card');
-                      songItem.innerHTML = `
-                          <img src="${imageUrl}" alt="${song.data.name}" class="song-image" />
-                          <h3>${song.data.name}</h3>
-                          <p>Artista: ${song.data.artists.items.map((artist) => artist.profile.name).join(', ')}</p>
-                      `;
-                      songList.appendChild(songItem);
-                  } else {
-                      console.log(`Canción ${index} no tiene los datos esperados.`);
-                  }
-              });
+    const currentTime = Date.now();
+    if (currentTime - lastSearchTime < searchDelay) {
+      console.log('Espera un momento antes de realizar otra búsqueda.');
+      return;
+    }
+    try {
+      let url = `https://spotify23.p.rapidapi.com/search/?q=${search}&type=tracks&offset=0&limit=8&numberOfTopResults=5`;
+      let res = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '27affe6731msh64d1fa5c477d18dp1f41d5jsn3f8bf97a8340',
+          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+        },
+      });
+      let data = await res.json();
+      console.log(data);
+      if (data && data.tracks && data.tracks.items) {
+        songList.innerHTML = '';
+        data.tracks.items.forEach((song, index) => {
+          console.log(`Processing song ${index}`, song);
+          console.log('Detalles de la canción:', song);
+          if (song && song.data && song.data.name && song.data.artists) {
+            let imageUrl = song.data.albumOfTrack?.coverArt?.sources[0]?.url || '';
+            let songItem = document.createElement('div');
+            songItem.classList.add('song-card');
+            songItem.innerHTML = `
+              <img src="${imageUrl}" alt="${song.data.name}" class="song-image" data-uri="${song.data.uri}" />
+              <h3>${song.data.name}</h3>
+              <p>Artista: ${song.data.artists.items.map((artist) => artist.profile.name).join(', ')}</p>
+            `;
+            songList.appendChild(songItem);
+            songItem.querySelector('.song-image').addEventListener('click', (event) => {
+              const uri = event.target.getAttribute('data-uri');
+              frame.setAttribute('uri', uri);
+            });
           } else {
-              console.log('No se encontraron resultados válidos en la respuesta de la API.');
+            console.log(`Canción ${index} no tiene los datos esperados.`);
           }
-      } catch (error) {
-          console.log(`Error: ${error}`);
+        });
+      } else {
+        console.log('No se encontraron resultados válidos en la respuesta de la API.');
       }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
 
-      lastSearchTime = currentTime;
+    lastSearchTime = currentTime;
   }
+
   searchInput.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-          let search = searchInput.value.trim();
-          if (search) {
-              searchSpotify(search);
-          } else {
-              songList.innerHTML = '';
-          }
+    if (event.key === 'Enter') {
+      let search = searchInput.value.trim();
+      if (search) {
+        searchSpotify(search);
+      } else {
+        songList.innerHTML = '';
       }
+    }
   });
 });
+
+
+///////////////////////////////// ALBUMS ////////////////////////////////////////////7
+
